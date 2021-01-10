c11b3d5fad302d4b48118c45729245ebf1bdf9b8
KeganH <keganhollern@gmail.com>
2021-01-10 16:29:51 -0500

    Added putChar Added writeFile Added fileExists Added deleteFile

--- out/build/x64-Debug/git_sha1.cpp
+++ out/build/x64-Debug/git_sha1.cpp
@@ -1,2 +1,2 @@
-#define GIT_SHA1 "0fc2fc210ea391837d989ba3fe7da2f71bbec074"
+#define GIT_SHA1 "5cbcc4d53dfb2f5792d95fdf0af6bd1e35b60602"
 extern const char g_GIT_SHA1[] = GIT_SHA1;
--- src/sysqf/filesystemcmds.cpp
+++ src/sysqf/filesystemcmds.cpp
@@ -11,9 +11,10 @@
 
 using namespace sqf;
 namespace {
+	
+	// low level file operations
 	int stream_count;
 	std::map<int, FILE*> open_file_streams;
-
 	value open_file(virtualmachine* vm, value::cref right) //_handle = openFile ["file_name.txt", "r"];
 	{
 		auto arr = right.as_vector();
@@ -70,6 +71,48 @@ namespace {
 
 		return std::fgetc(pFile);
 	}
+	value put_char(virtualmachine* vm, value::cref left, value::cref right) //_handle putChar 32; //put a space
+	{
+		int index = left.as_int();
+
+		auto res = open_file_streams.find(index);
+		if (res == open_file_streams.end())
+		{
+			vm->err() << "Invalid file handle: " << index << "." << std::endl;
+			return false;
+		}
+
+		FILE* pFile = open_file_streams[index];
+
+		if (!pFile)
+		{
+			vm->err() << "Invalid file handle: " << index << "." << std::endl;
+			return false;
+		}
+
+		//extract value from right param
+		int value;
+		if (right.dtype() == sqf::type::SCALAR)
+			value = right.as_int();
+		else if (right.dtype() == sqf::type::STRING)
+		{
+			auto stringval = right.as_string();
+			if (stringval.size() != 1)
+			{
+
+				return false;
+			}
+			value = (int)stringval.c_str()[0];
+		}
+		else
+		{
+			vm->err() << "Character element was expected to be STRING or SCALAR, got " << sqf::type_str(right.dtype()) << '.' << std::endl;
+			return false;
+		}
+
+		std::fputc(value, pFile);
+		return true;
+	}
 	value endof_file(virtualmachine* vm, value::cref right)
 	{
 		int index = right.as_int();
@@ -115,6 +158,7 @@ namespace {
 		return {};
 	}
 
+	//more usefull functionality
 	value read_file(virtualmachine* vm, value::cref right) //_bytes = readFile "file_name.txt";
 	{
 		auto filename = right.as_string();
@@ -135,15 +179,69 @@ namespace {
 		vm->err() << "Failed to read file: " << filename << "." << std::endl;
 		return std::vector<value>(); //return empty array when fail to read
 	}
+	value write_file(virtualmachine* vm, value::cref right) //writeFile ["file.txt", [12,12,12,12]];
+	{
+		auto arr = right.as_vector();
+		if (arr.size() != 2)
+		{
+			vm->err() << "Expected 2 arguments!" << std::endl;
+			return false;
+		}
+		if (arr[0].dtype() != sqf::type::STRING)
+		{
+			vm->err() << "First element of array was expected to be STRING, got " << sqf::type_str(arr[0].dtype()) << '.' << std::endl;
+			return false;
+		}
+		if (arr[1].dtype() != sqf::type::ARRAY)
+		{
+			vm->err() << "Second element of array was expected to be ARRAY, got " << sqf::type_str(arr[1].dtype()) << '.' << std::endl;
+			return false;
+		}
+		auto filename = arr[0].as_string();
+		auto data = arr[1].as_vector();
+
+		std::ofstream stream(filename.c_str(), std::ios::out | std::ios::binary);
+		if (!stream.is_open())
+		{
+			vm->err() << "Failed to open file: " << filename << "." << std::endl;
+			return false;
+		}
+		std::vector<char> content(data.begin(), data.end());
+		stream.write((const char*)&content[0], content.size());
+		stream.close();
+
+		return true;
+	}
+	value file_exists(virtualmachine* vm, value::cref right)
+	{
+		auto filename = right.as_string();
+		std::ifstream f(filename.c_str());
+		return f.good();
+	}
+	value delete_file(virtualmachine* vm, value::cref right)
+	{
+		auto filename = right.as_string();
+		if (std::remove(filename.c_str()) == 0)
+			return true;
+		else
+			vm->err() << "Failed to remove file!" << std::endl;
+
+		return false;
+	}
 }
 void sqf::commandmap::initfilesystemcmds()
 {
 	//file stream handling
 	add(unary("openFile", sqf::type::ARRAY, "Opens a file stream with the specified mode.", open_file));
 	add(unary("isEndOf", sqf::type::SCALAR, "Tests the end-of-file indicator for the given stream.", endof_file));
+	add(binary(4, "putChar", sqf::type::SCALAR, sqf::type::SCALAR, "Writes a character to the given stream.", put_char)); //note: putChar has two commands, differing RParam.
+	add(binary(4, "putChar", sqf::type::SCALAR, sqf::type::STRING, "Writes a character to the given stream.", put_char));
 	add(unary("readChar", sqf::type::SCALAR, "Gets the next character from the specified stream.", get_char));
 	add(unary("closeFile", sqf::type::SCALAR, "Gets the next character from the specified stream.", close_file));
 
 	//useful file functionality
 	add(unary("readFile", sqf::type::STRING, "Read file bytes to array (does not support wide characters)", read_file));
+	add(unary("writeFile", sqf::type::ARRAY, "Write file bytes", write_file));
+	add(unary("fileExists", sqf::type::STRING, "Checks if a file exists", file_exists));
+	add(unary("deleteFile", sqf::type::STRING, "Deletes the specified file", delete_file));
 }
